d92c6a32867561537efaaebd4ef37cc1309996da
Simon Hafner <hafnersimon@gmail.com>
2013-04-24 10:05:11 -0500

    next stage - supervised

--- src/main/scala/gpp/Supervised.scala
+++ src/main/scala/gpp/Supervised.scala
@@ -18,12 +18,36 @@ object Supervised extends Method {
   }
 }
 
-object SimpleFeatures extends Featurizer[String, String]{
+class FeatureCollector(feats: List[Feature]) extends Featurizer[String, String]{
   def apply(raw: String) = {
     val parsed = util.POSTagger(raw)
-    val features = scala.collection.mutable.ListBuffer[String]()
-    features.appendAll(parsed.flatMap(token => List("token=" + token.token.toLowerCase, "tag=" + token.tag, "token+tag=" + token.token + "+" + token.tag)))
-    features.appendAll(parsed.map(util.MPQA(_).map(_.toString)).flatten)
-    features.map(FeatureObservation(_))
+    feats.flatMap(_.apply(parsed))
   }
+}
+object Tokens extends Feature {
+  def apply(parsed: List[util.Token]): List[FeatureObservation[String]] = parsed.map(token => "token=" + token.token.toLowerCase).map(FeatureObservation(_))
+}
+
+object Tags extends Feature {
+  def apply(parsed: List[util.Token]): List[FeatureObservation[String]] = parsed.map(token => "tag=" + token.tag).map(FeatureObservation(_))
+}
+
+object TokensnTags extends Feature {
+  def apply(parsed: List[util.Token]): List[FeatureObservation[String]] = parsed.map(token => "token+tag=" + token.token.toLowerCase + "+" + token.tag).map(FeatureObservation(_))
+}
+
+object TagBigrams extends Feature {
+  def apply(parsed: List[util.Token]): List[FeatureObservation[String]] = parsed.sliding(2).map(list => list.map(_.tag).mkString("bigramTags=", "+", "")).map(FeatureObservation(_)).toList
+}
+
+object TokenBigrams extends Feature {
+  def apply(parsed: List[util.Token]): List[FeatureObservation[String]] = parsed.sliding(2).map(list => list.map(_.token.toLowerCase).mkString("bigramTokens=", "+", "")).map(FeatureObservation(_)).toList
+}
+
+object MPQALex extends Feature {
+  def apply(parsed: List[util.Token]): List[FeatureObservation[String]] = parsed.map(util.MPQA(_).map(_.toString)).flatten.map(FeatureObservation(_)).toList
+}
+
+object Numbers extends Feature {
+  def apply(parsed: List[util.Token]): List[FeatureObservation[String]] = parsed.map(token => """([+-])\d""".r.findFirstIn(token.token).map(_(0).toString)).flatten.map(FeatureObservation(_)).toList
 }
--- src/main/scala/gpp/gpp.scala
+++ src/main/scala/gpp/gpp.scala
@@ -12,12 +12,24 @@ trait Method {
   def apply(featurizer: Featurizer[String, String], train: Seq[Example[String, String]], cost: Double): Model
 }
 
+trait Feature {
+  def apply(parsed: List[util.Token]): List[FeatureObservation[String]]
+}
+
 object Opts {
   import org.rogach.scallop._
   val methods = Map[String, Method](
     "majority" -> Majority,
     "lexicon" -> Lexicon,
     "L2R_LR" -> Supervised
+  )
+  val feats = Map[String, Feature](
+    "tokens" -> Tokens,
+    "tags" -> Tags,
+    "tokensntags" -> TokensnTags,
+    "tagbigrams" -> TagBigrams,
+    "tokenbigrams" -> TokenBigrams,
+    "MPQA" -> MPQALex
   )
   val version = "0.0.0"
 
@@ -31,11 +43,14 @@ For usage see below:
     // --help is baked into scallop, can't use short.
     val verbose = opt[Boolean]("verbose")
     val detailed = opt[Boolean]("detailed")
-    val extended = opt[Boolean]("extended", short='x', descr="Use extended features.")
+    val extended = opt[Boolean]("extended", short='x', descr="Use extended features. A shortcut for selecting all features. -f is ignored.")
     val eval = opt[String]("eval", required = true, descr = "The files containing evalualation events.")
     val method = opt[String]("method", descr = "The type of solver to use. Possible values: " + methods.keySet.mkString(", "), validate = methods.keySet, required = true)
     val train = opt[String]("train", required = true, descr = "The files containing training events.")
     val cost = opt[Double]("cost", descr = "The cost parameter C. Bigger values means less regularization (more fidelity to the training set). (default = 1.0)", default = Some(1.0))
+    val fts = opt[List[String]]("features", descr = "The features to use. Combine as needed: " + feats.keySet.mkString(", "))
+    // Somehow defaults for List[A] don't work as expected
+    lazy val features = if (fts().size == 0) List("tokens") else fts()
   }
 }
 
@@ -44,7 +59,7 @@ object Exp {
     val opts = Opts(args)
     val method = Opts.methods(opts.method())
     val trainEx = parseXML(opts.train())
-    val featurizer = SimpleFeatures
+    val featurizer = if(opts.extended()) new FeatureCollector(Opts.feats.values.toList) else new FeatureCollector(opts.features.map(Opts.feats))
     val model = method(featurizer, trainEx, opts.cost())
     val evalEx = parseXML(opts.eval())
     val results = evalEx.map(model)
