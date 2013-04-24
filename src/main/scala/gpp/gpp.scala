9541ae53959ea56a02bf6f263271a103467d5d85
Simon Hafner <hafnersimon@gmail.com>
2013-04-24 10:14:16 -0500

    now supports multiple training files

--- src/main/scala/gpp/gpp.scala
+++ src/main/scala/gpp/gpp.scala
@@ -42,13 +42,13 @@ Classification application version $version
 For usage see below:
 	     """)
     // --help is baked into scallop, can't use short.
-    val verbose = opt[Boolean]("verbose")
-    val detailed = opt[Boolean]("detailed")
-    val extended = opt[Boolean]("extended", short='x', descr="Use extended features. A shortcut for selecting all features. -f is ignored.")
-    val eval = opt[String]("eval", required = true, descr = "The files containing evalualation events.")
-    val method = opt[String]("method", descr = "The type of solver to use. Possible values: " + methods.keySet.mkString(", "), validate = methods.keySet, required = true)
-    val train = opt[String]("train", required = true, descr = "The files containing training events.")
-    val cost = opt[Double]("cost", descr = "The cost parameter C. Bigger values means less regularization (more fidelity to the training set). (default = 1.0)", default = Some(1.0))
+    val verbose = opt[Boolean]()
+    val detailed = opt[Boolean]()
+    val extended = opt[Boolean](short='x', descr="Use extended features. A shortcut for selecting all features. -f is ignored.")
+    val eval = opt[String](required = true, descr = "The files containing evalualation events.")
+    val method = opt[String](descr = "The type of solver to use. Possible values: " + methods.keySet.mkString(", "), validate = methods.keySet, required = true)
+    val train = opt[List[String]](required = true, descr = "The files containing training events.")
+    val cost = opt[Double](descr = "The cost parameter C. Bigger values means less regularization (more fidelity to the training set). (default = 1.0)", default = Some(1.0))
     val fts = opt[List[String]]("features", descr = "The features to use. Combine as needed: " + feats.keySet.mkString(", "))
     // Somehow defaults for List[A] don't work as expected
     lazy val features = if (fts().size == 0) List("tokens") else fts()
@@ -59,9 +59,9 @@ object Exp {
   def main(args: Array[String]) {
     val opts = Opts(args)
     val method = Opts.methods(opts.method())
-    val trainEx = parseXML(opts.train())
+    val trainEx = opts.train().map(parseXML(_))
     val featurizer = if(opts.extended()) new FeatureCollector(Opts.feats.values.toList) else new FeatureCollector(opts.features.map(Opts.feats))
-    val model = method(featurizer, trainEx, opts.cost())
+    val model = method(featurizer, trainEx.flatten, opts.cost())
     val evalEx = parseXML(opts.eval())
     val results = evalEx.map(model)
     println(ConfusionMatrix(evalEx.map(_.label), results, evalEx.map(_.features)))
